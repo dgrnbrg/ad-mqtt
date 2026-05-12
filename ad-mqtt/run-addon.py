8e234ea8786fd868dbde1c9060c97a85129d9e73
David Greenberg <dsg123456789@gmail.com>
2026-05-12 13:04:22 -0400

    Add homeassistant API too and log that this version is running

--- ad-mqtt/run-addon.py
+++ ad-mqtt/run-addon.py
@@ -16,6 +16,7 @@ cfg.alarm.port = options['alarm'].get('port', 10000)
 # To reset all zones to closed (not faulted) on startup, set this to True
 cfg.alarm.restore_on_startup = options['alarm'].get('restore_on_startup', True)
 
+print("With new API deps added")
 # MQTT Broker connection
 cfg.mqtt.broker = os.environ['MQTT_HOST']
 cfg.mqtt.port = int(os.environ['MQTT_PORT'])
